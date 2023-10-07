020e3c958ca66efd20bafc94622beb535cc77f14
Thienng23 <21522626@gm.uit.edu.vn>
2023-10-07 18:22:21 +0700

    Lab1

---
+++ LAB01/app/src/main/java/com/example/lab01/MainActivity1B.java
@@ -0,0 +1,14 @@
+package com.example.lab01;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity1B extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_activity1_b);
+    }
+}
