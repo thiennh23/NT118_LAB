ee48f606faa99c6fcb9fe4ab5a07a1ad123bfd27
Nguyễn Hữu Thiện <135424236+thiennh23@users.noreply.github.com>
2023-12-27 20:27:49 +0700

    DONE FIRST TASK IN LAB06 BUT HAVE NOT TESTED YET BECAUSE OF THE LACK OF DEVICES

--- LAB06/app/src/main/java/com/example/lab06/MainActivity.java
+++ LAB06/app/src/main/java/com/example/lab06/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.lab06;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button btn1 = findViewById(R.id.bai1);
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, Bai1.class);
+                startActivity(intent);
+            }
+        });
     }
 }
