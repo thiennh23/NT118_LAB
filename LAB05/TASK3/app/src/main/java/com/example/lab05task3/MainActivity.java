fb1e2d74ce49ecc0f9c4c22acb2ff2e155c86db9
Nguyễn Hữu Thiện <135424236+thiennh23@users.noreply.github.com>
2023-12-27 23:14:59 +0700

    DONE TASK3 LAB05

--- LAB05/TASK3/app/src/main/java/com/example/lab05task3/MainActivity.java
+++ LAB05/TASK3/app/src/main/java/com/example/lab05task3/MainActivity.java
@@ -2,13 +2,50 @@ package com.example.lab05task3;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Button btnQuickJob, btnSlowJob;
+    private TextView tvStatus;
+    private SlowTask slowTask;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        findViewByIds();
+        // Init slowtask
+        slowTask = new SlowTask(MainActivity.this, tvStatus);
+        // Handle onClickListenner
+        btnQuickJob.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
+                tvStatus.setText(sdf.format(new Date()));
+            }
+        });
+        btnSlowJob.setOnClickListener(
+                v -> {
+                    if (slowTask == null || slowTask.getStatus() != AsyncTask.Status.RUNNING) {
+                        // Create a new instance of SlowTask and execute it
+                        slowTask = new SlowTask(MainActivity.this, tvStatus);
+                        slowTask.execute();
+                    }
+                }
+        );
     }
+
+    private void findViewByIds() {
+        btnQuickJob = (Button) findViewById(R.id.btn_quick_job);
+        btnSlowJob = (Button) findViewById(R.id.btn_slow_job);
+        tvStatus = (TextView) findViewById(R.id.tv_status);
+    }
+
 }
