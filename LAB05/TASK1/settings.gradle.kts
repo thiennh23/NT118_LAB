bd220e39cf9dd2e482ddd687e38c9c76fd2a70cc
Nguyễn Hữu Thiện <135424236+thiennh23@users.noreply.github.com>
2023-12-27 22:32:28 +0700

    ININT LAB05 TASK1

---
+++ LAB05/TASK1/settings.gradle.kts
@@ -0,0 +1,17 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "LAB05TASK1"
+include(":app")
